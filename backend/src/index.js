041eeb27b5acaf826aa226d91cb379481628a559
Dota43ver <facundoagueroc@gmail.com>
2024-07-12 11:38:54 -0300

    asd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import AppDataSource from './config/db';
+import 'dotenv/config';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+AppDataSource.initialize()
+	.then(() => {
+		console.log('Connected to the database!');
+
+		// Aquí puedes iniciar tu servidor o cualquier lógica de tu aplicación
+		app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	})
+	.catch((error) => console.log('Error connecting to the database:', error));
