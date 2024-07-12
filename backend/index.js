614e972d19f43b94694eb5d1c1b115c9cda4bc33
Fernando Rodriguez <frodriguezv06@gmail.com>
2024-07-12 09:23:02 -0500

    fix merge

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,21 @@
-import express from "express";
-import AppDataSource from "./src/config/db.js";
-import userRoutes from "./src/routes/user.routes.js";
+import express from 'express';
+import AppDataSource from './src/config/db.js';
+import userRoutes from './src/routes/user.routes.js';
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/users", userRoutes);
+app.use('/api/v1/users', userRoutes);
 
 AppDataSource.initialize()
-  .then(() => {
-    console.log("Connected to the database!");
+	.then(() => {
+		console.log('Connected to the database!');
 
-    
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((error) => console.log("Error connecting to the database:", error));
+		app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	})
+	.catch((error) => console.log('Error connecting to the database:', error));
