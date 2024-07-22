19ac678da5059a0678bef0130c4ff37b7940e05c
Dota43ver <facundoagueroc@gmail.com>
2024-07-22 17:43:31 -0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import AppDataSource from './src/config/db.js';
 import userRoutes from './src/routes/user.routes.js';
-import cors from 'cors';
+import 'dotenv/config';
+// import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
